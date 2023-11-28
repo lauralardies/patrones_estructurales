0296049bcc5069e8f6305c57e8019bad43ac6754
lauralardies <lrodrlar@myuax.com>
2023-11-28 10:04:07 +0100

    avanzamos

--- Patrones Estructurales/Ejercicio 2/composite.py
+++ Patrones Estructurales/Ejercicio 2/composite.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List
+import csv
+import datetime
 
 
 class Component(ABC):
@@ -23,22 +25,39 @@ class Component(ABC):
         pass
 
     @abstractmethod
-    def access(self) -> None:
+    def list_element(self) -> str:
+        pass
+
+    @abstractmethod
+    def access(self):
         pass
 
 
 class Documento(Component):
-    def __init__(self, nombre, tipo, tam) -> None:
+    def __init__(self, nombre, contenido, tipo, tam, sensible=False) -> None:
         self._nombre = nombre
+        self._contenido = contenido
         self._tipo = tipo
         self._tam = tam
+        self._sensible = sensible
 
     def tam(self) -> str:
         return self._tam
     
-    def access(self) -> str:
+    def list_element(self) -> str:
         return self._nombre
-
+    
+    def access(self):
+        if self._sensible:
+            print("El documento es sensible, se va a registrar su acceso. Facilite los siguientes datos:")
+            nombre = input("Nombre: ")
+            apellido = input("Apellido: ")
+            dni = input("DNI: ")
+            datos = [nombre, apellido, dni, self._nombre, datetime.datetime.now()]
+            with open("Patrones Estructurales/Ejercicio 2/accesos/registro_accesos.csv", "a", newline="") as archivo:
+                writer = csv.writer(archivo)
+                writer.writerow(datos)
+        return self._contenido
 
 class Enlace(Component):
     def __init__(self, ruta, tam) -> None:
@@ -48,7 +67,7 @@ class Enlace(Component):
     def tam(self) -> str:
         return self._tam
     
-    def access(self) -> str:
+    def list_element(self) -> str:
         return self._nombre
 
 
@@ -67,5 +86,5 @@ class Carpeta(Component):
     def tam(self) -> str:
         return sum([x.tam() for x in self._children])
     
-    def access(self) -> None:
+    def list_element(self) -> None:
         return [x.access() for x in self._children]
