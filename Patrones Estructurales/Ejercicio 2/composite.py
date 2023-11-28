6eb8808faccf328bd5f1a9047d1b7cad9d764ab3
lauralardies <lrodrlar@myuax.com>
2023-11-28 10:29:40 +0100

    c

--- Patrones Estructurales/Ejercicio 2/composite.py
+++ Patrones Estructurales/Ejercicio 2/composite.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List
+from config import limpiar_pantalla
 import csv
 import datetime
 
@@ -40,6 +41,37 @@ class Documento(Component):
         self._tipo = tipo
         self._tam = tam
         self._sensible = sensible
+        self._access = False # Variable que indica si estás dentro del documento o no
+    
+    def add(self):
+        if self._access: # Sólo lo pudes modificar si ya has registrado tu acceso
+            print("Este es el contenido de su documento: \n\n" + self._contenido)
+            input("Pulse cualquier tecla para continuar...")
+            limpiar_pantalla()
+            print("Introduzca el texto que desea añadir al documento, se añadirá al final del mismo: ")
+            texto = input(">> ")
+            self._contenido += texto
+            limpiar_pantalla()
+            print("Se ha añadido el texto correctamente.\n\nEste es el contenido de su documento actualizado: \n\n" + self._contenido)
+            input("Pulse cualquier tecla para continuar...")
+        else:
+            print("No puede modificar el documento si no ha registrado su acceso previamente.")
+            input("Pulse cualquier tecla para continuar...")
+
+    def remove(self):
+        if self._access: # Sólo lo pudes modificar si ya has registrado tu acceso
+            print("Este es el contenido de su documento: \n\n" + self._contenido)
+            input("Pulse cualquier tecla para continuar...")
+            limpiar_pantalla()
+            print("Introduzca el texto que desea eliminar del documento: ")
+            texto = input(">> ")
+            self._contenido = self._contenido.replace(texto, "")
+            limpiar_pantalla()
+            print("Se ha eliminado el texto correctamente.\n\nEste es el contenido de su documento actualizado: \n\n" + self._contenido)
+            input("Pulse cualquier tecla para continuar...")
+        else:
+            print("No puede modificar el documento si no ha registrado su acceso previamente.")
+            input("Pulse cualquier tecla para continuar...")
 
     def tam(self) -> str:
         return self._tam
@@ -49,14 +81,23 @@ class Documento(Component):
     
     def access(self):
         if self._sensible:
-            print("El documento es sensible, se va a registrar su acceso. Facilite los siguientes datos:")
-            nombre = input("Nombre: ")
-            apellido = input("Apellido: ")
-            dni = input("DNI: ")
+            while True:
+                limpiar_pantalla()
+                print("El documento es sensible, se va a registrar su acceso. Facilite los siguientes datos:")
+                nombre = input("Nombre: ")
+                apellido = input("Apellido: ")
+                dni = input("DNI: ")
+                if nombre.isalpha() and apellido.isalpha() and dni.isalnum() and len(dni) == 9:
+                    break
+                else:
+                    limpiar_pantalla()
+                    print("Los datos introducidos no son válidos, inténtelo de nuevo.")
+                    print("Asegúrese de no introducir espacios y de que el DNI tenga 8 dígitos y 1 letra.")
             datos = [nombre, apellido, dni, self._nombre, datetime.datetime.now()]
             with open("Patrones Estructurales/Ejercicio 2/accesos/registro_accesos.csv", "a", newline="") as archivo:
                 writer = csv.writer(archivo)
                 writer.writerow(datos)
+        self._access = True
         return self._contenido
 
 class Enlace(Component):
